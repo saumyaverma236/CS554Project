5607c999e34c69624e2447c9c051576032c35711
HansiMamidi <mhansi3107@gmail.com>
2023-12-06 19:38:24 -0500

    'firebase'

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import '../App.css';
+import axios from 'axios';
+
+function Dashboard() {
+  const { currentUser } = useContext(AuthContext);
+
+  const spotifySignOn = async () => {
+    try {
+      const { data } = await axios.get(
+        'http://localhost:3000/users/login'
+      );
+      console.log('data', data);
+
+      const redirectUri = encodeURIComponent('http://localhost:5173/dashboard');
+      const spotifyAuthUrl = `${data}&redirect_uri=${redirectUri}`;
+
+      window.location.href = spotifyAuthUrl;
+
+      setLoading(false);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className='card'>
+      <h2>
+        Hello {currentUser && currentUser.displayName}, this is the Protected Home page
+      </h2>
+      <img
+        onClick={() => spotifySignOn()}
+        alt='spotify signin'
+        src='/imgs/btn_spotify_signin.png'
+      />
+    </div>
+  );
+}
+
+export default Dashboard;
