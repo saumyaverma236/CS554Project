b73a55fcc3645714548475bdcd5f645a9cfd79e8
Aditya Gupta <kuku.g.r@gmail.com>
2023-12-14 16:17:57 -0500

    added data config files

---
+++ backend/config/mongoCollection.js
@@ -0,0 +1,24 @@
+import {dbConnection} from './mongoConnection.js';
+
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+
+export const users = getCollectionFn('users');
+export const cpu = getCollectionFn('cpu');
+export const gpu = getCollectionFn('gpu');
+export const mobo = getCollectionFn('mobo');
+export const psu = getCollectionFn('psu');
+export const ram = getCollectionFn('ram');
+export const disk = getCollectionFn('disk');
