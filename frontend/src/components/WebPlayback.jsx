eceaab900f2b24b4ff846f2a60df2382077fbc33
Parshotan Seenanan <parshsee@gmail.com>
2023-12-14 23:43:37 -0500

    Added code to add songs to queue

--- frontend/src/components/WebPlayback.jsx
+++ frontend/src/components/WebPlayback.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from 'react';
 import SearchModal from './search/SearchModal';
 
+import DeviceContext from '../context/DeviceContext';
+
 const track = {
 	name: "",
 	album: {
@@ -79,6 +81,7 @@ function WebPlayback(props) {
 	if (!is_active) {
 		return (
 			<>
+			<DeviceContext.Provider value={{ deviceID: deviceID.current }}>
 				<div className="container">
 					<div className="main-wrapper">
 						<b> Instance not active. Transfer your playback using your Spotify app </b>
@@ -97,46 +100,49 @@ function WebPlayback(props) {
 						/>
 					)}
 				</div>
+			</DeviceContext.Provider>
 			</>)
 	} else {
 		return (
 			<>
-				<div className="container">
-					<div className="main-wrapper">
+				<DeviceContext.Provider value={{ deviceID: deviceID.current }}>
+					<div className="container">
+						<div className="main-wrapper">
 
-						<img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
+							<img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
 
-						<div className="now-playing__side">
-							<div className="now-playing__name">{current_track.name}</div>
-							<div className="now-playing__artist">{current_track.artists[0].name}</div>
+							<div className="now-playing__side">
+								<div className="now-playing__name">{current_track.name}</div>
+								<div className="now-playing__artist">{current_track.artists[0].name}</div>
 
-							<button className="btn-spotify" onClick={() => { player.previousTrack() }} >
-								&lt;&lt;
-							</button>
+								<button className="btn-spotify" onClick={() => { player.previousTrack() }} >
+									&lt;&lt;
+								</button>
 
-							<button className="btn-spotify" onClick={() => { player.togglePlay() }} >
-								{is_paused ? "PLAY" : "PAUSE"}
-							</button>
+								<button className="btn-spotify" onClick={() => { player.togglePlay() }} >
+									{is_paused ? "PLAY" : "PAUSE"}
+								</button>
 
-							<button className="btn-spotify" onClick={() => { player.nextTrack() }} >
-								&gt;&gt;
-							</button>
+								<button className="btn-spotify" onClick={() => { player.nextTrack() }} >
+									&gt;&gt;
+								</button>
+							</div>
+
+							<div>
+								<button onClick={() => { setShowSearchModal(true); }}>
+									Search for Songs
+								</button>
+							</div>
+
+							{showSearchModal && (
+								<SearchModal 
+									isOpen={showSearchModal}
+									handleClose={handleCloseModals}
+								/>
+							)}
 						</div>
-
-						<div>
-							<button onClick={() => { setShowSearchModal(true); }}>
-								Search for Songs
-							</button>
-						</div>
-
-						{showSearchModal && (
-							<SearchModal 
-								isOpen={showSearchModal}
-								handleClose={handleCloseModals}
-							/>
-						)}
 					</div>
-				</div>
+				</DeviceContext.Provider>
 			</>
 		);
 	}
