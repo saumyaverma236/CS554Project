560b34f7856e5c3303f40e4f14ef25443b536ef4
HansiMamidi <118401400+HansiMamidi@users.noreply.github.com>
2023-12-17 22:54:04 +0000

    setting.js

--- backend/config/setting.js
+++ backend/config/setting.js
@@ -4,7 +4,6 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 
 export const mongoConfig = {
-    serverUrl: "mongodb://127.0.0.1/",
+    serverUrl: "mongodb+srv://adityagupta17:cdPkVQ2Cm9IY1esl@ppc0.opyszgr.mongodb.net/",
     database: 'MusicMates'
   };
-
