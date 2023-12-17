8891efa92783c7aac5ce268e90c3321b4714c559
saumyaverma236 <saumyaverma236@gmail.com>
2023-12-16 23:29:44 -0500

    made changes in backendstuff branch

--- backend/config/setting.js
+++ backend/config/setting.js
@@ -5,6 +5,6 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 export const mongoConfig = {
     serverUrl: "mongodb+srv://adityagupta17:cdPkVQ2Cm9IY1esl@ppc0.opyszgr.mongodb.net/",
-    database: 'Aditya_Gupta_lab10'
+    database: 'MusicMates'
   };
 
