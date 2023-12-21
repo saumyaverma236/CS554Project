cfc2bfd4025a16c1900aa7336a3639645dd565d6
saumyaverma236 <saumyaverma236@gmail.com>
2023-12-20 22:07:10 -0500

    NavBar

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,29 +1,40 @@
 // Dashboard.js
 import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, TextField, Typography, Box } from '@mui/material';
+import { AppBar,Toolbar,Button, TextField, Typography, Box } from '@mui/material';
 import { AuthContext } from '../context/AuthContext';
 import '../App.css';
 import CreateRoomModal from './createRoomModal';
 import axios from 'axios';
 import SignOut from './SignOut';
 import PublicRooms from './PublicRooms';
+import NavigationBar from './NavigationBar';
 
 function Dashboard() {
-  const [isModalOpen, setModalOpen] = useState(false);
   const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+  const [currentView, setCurrentView] = useState('dashboard');
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [accessToken, setAccessToken] = useState(undefined);
+  const [publicRoomsData, setPublicRoomsData] = useState(undefined);
+  const [roomCode, setRoomCode] = useState('');
+
 
   const handleModalClose = () => {
     setModalOpen(false);
   };
-  const { currentUser } = useContext(AuthContext);
 
-  const [accessToken, setAccessToken] = useState(undefined);
   const [loading, setLoading] = useState(false);
 
-  const [roomCode, setRoomCode] = useState('');
-  const [publicRoomsData, setPublicRoomsData] = useState(undefined);
   const [isPublicRoomsModalOpen, setPublicRoomsModalOpen] = useState(false);
+  const [showPublicRooms, setShowPublicRooms] = useState(false);
+
+  const handleViewChange = (view) => {
+    setCurrentView(view);
+    if (view === 'publicRooms' && !publicRoomsData) {
+      handlePublicRooms();
+    }
+  };
 
   const handleRoomCodeChange = (event) => {
     setRoomCode(event.target.value);
@@ -45,10 +56,6 @@ function Dashboard() {
     }
   };
 
-  const handlePublicRoomsClose = () => {
-    console.log("handling public rooms close")
-    setPublicRoomsModalOpen(false);
-  };
 
   const handlePublicRooms = async () => {
     try {
@@ -56,7 +63,8 @@ function Dashboard() {
       const rooms = response.data;
       console.log("PUBLIC ROOMS:", rooms)
       setPublicRoomsData(rooms);
-      setPublicRoomsModalOpen(true);
+      // setPublicRoomsModalOpen(true);
+     // setShowPublicRooms(true);
     } catch (error) {
       console.log(error);
       alert(error);
@@ -105,82 +113,61 @@ function Dashboard() {
   }, [accessToken]);
 
   return (
-    <div className='card' style={{ backgroundColor: '#ffe4e1', padding: '20px', textAlign: 'center' }}>
-      <Typography variant="h1" style={{ color: '#008080' }}>Music Mates</Typography>
-      <Typography variant="h2" style={{ color: '#ff7f50' }}>Welcome back, {currentUser && currentUser.displayName}!</Typography>
-      <Typography variant="body1">It's great to see you again.</Typography>
-      
-      <Button 
-        onClick={handlePublicRooms}
-        style={{ 
-          backgroundColor: '#ff7f50', 
-          color: 'white', 
-          padding: '10px 20px', 
-          width: '200px', 
-          margin: '10px auto', 
-          display: 'block' 
-        }}
-        variant="contained"
-      >
-        Browse Public Rooms
-      </Button>
-      {publicRoomsData && publicRoomsData.length > 0 && (
-    <PublicRooms
-      publicRooms={publicRoomsData}
-      onClose={handlePublicRoomsClose}
-      onCardClick={(room) => {
-        console.log('Clicked on room:', room);
-        // Add logic to handle the clicked card
-      }}
-    />
-  )}
-      <Button 
-        onClick={handleCreateRoom} 
-        style={{ 
-          backgroundColor: '#40e0d0', 
-          color: 'white', 
-          padding: '10px 20px', 
-          width: '200px', 
-          margin: '10px auto', 
-          display: 'block' 
-        }}
-        variant="contained"
-      >
-        Create Room
-      </Button>
+    <div>
+      <NavigationBar currentView={currentView} onNavigate={handleViewChange} />
 
-      <CreateRoomModal currentUser={currentUser} isOpen={isModalOpen} onClose={handleModalClose} />
+      {currentView === 'dashboard' && (
+        <div className='card' style={{ backgroundColor: '#ffe4e1', padding: '20px', textAlign: 'center' }}>
+          <Typography variant="h1" style={{ color: '#008080' }}>Music Mates</Typography>
+          <Typography variant="h2" style={{ color: '#ff7f50' }}>Welcome back, {currentUser && currentUser.displayName}!</Typography>
+          <Typography variant="body1">It's great to see you again.</Typography>
+          
+          <Button 
+            onClick={handleCreateRoom} 
+            style={{ backgroundColor: '#40e0d0', color: 'white', padding: '10px 20px', margin: '10px auto', display: 'block', width: '200px',height:'50px' }}
+            variant="contained"
+          >
+            Create Room
+          </Button>
 
-      <Box style={{ margin: '20px' }}>
-        <label style={{ color: '#008080' }}>Have a room code? Join a room with it:</label>
-        <TextField 
-          type="text" 
-          value={roomCode} 
-          onChange={handleRoomCodeChange}
-          style={{ margin: '10px' }}
-        />
-        <Button 
-          onClick={handleJoinRoom}
-          style={{ 
-            backgroundColor: '#ff7f50', 
-            color: 'white', 
-            padding: '10px 20px', 
-            width: '200px', 
-            margin: '10px auto', 
-            display: 'block' 
-          }}
-          variant="contained"
-        >
-          Join
-        </Button>
-      </Box>
+          <CreateRoomModal currentUser={currentUser} isOpen={isModalOpen} onClose={handleModalClose} />
 
-      {accessToken && (
-        <>
-          <Typography variant="h5" style={{ color: '#008080' }}>Wanna Logout? Click Below</Typography>
-          <SignOut />
-        </>
+          <Box style={{ margin: '20px' }}>
+            <label style={{ color: '#008080' }}>Have a room code? Join a room with it:</label>
+            <TextField 
+              type="text" 
+              value={roomCode} 
+              onChange={handleRoomCodeChange}
+              style={{ margin: '10px' }}
+            />
+            <Button 
+              onClick={handleJoinRoom}
+              style={{ backgroundColor: '#ff7f50', color: 'white', padding: '10px 20px', margin: '10px auto', display: 'block',width: '200px',height:'50px' }}
+              variant="contained"
+            >
+              Join
+            </Button>
+          </Box>
+
+          {accessToken && (
+            <>
+              <Typography variant="h5" style={{ color: '#008080' }}>Wanna Logout? Click Below</Typography>
+              <SignOut />
+            </>
+          )}
+        </div>
       )}
+
+      {currentView === 'publicRooms' && publicRoomsData && (
+        <PublicRooms
+          publicRooms={publicRoomsData}
+          // onCardClick={(room) => navigate(`/rooms/${room._id}`)}
+        />
+      )}
+
+      {/* {currentView === 'profile' && accessToken && (
+        // Profile content goes here
+      )} */}
     </div>
   );
 }
